2e607e62860a286367ce0123d6264b0fb40dd80b
Maxim Mitin <maximus405@gmail.com>
2022-01-23 19:36:06 +0300

    add activity indicator and some logic for UI

--- UrlSessionRewind/ViewController.swift
+++ UrlSessionRewind/ViewController.swift
@@ -11,23 +11,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
 
     @IBAction func getImagePressed(_ sender: Any) {
+        label.isHidden = true
+        getImageButton.isEnabled = false
+        activityIndicator.isHidden = true
+        activityIndicator.startAnimating()
+        
         guard let url = URL(string: "https://sun9-6.userapi.com/sun9-31/impf/c303314/v303314275/4312/x5z1ehgczp4.jpg?size=600x584&quality=96&sign=fb5338f1d87b54a1ec10682015fbf82c&type=album") else {return}
         
         let session = URLSession.shared
         session.dataTask(with: url) { data, response, error in
             if let data = data, let image = UIImage(data: data) {
                 DispatchQueue.main.async {
-                    self.label.isHidden = true
-                    self.getImageButton.isEnabled = false
+                    self.activityIndicator.stopAnimating()
                     self.imageView.image = image
                 }
             }
