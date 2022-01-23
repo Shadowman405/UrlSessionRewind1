d5f10e8348c17999f3f57bdf8657642b854b9b55
Maxim Mitin <maximus405@gmail.com>
2022-01-23 22:49:43 +0300

    post request - done

--- UrlSessionRewind/ViewController.swift
+++ UrlSessionRewind/ViewController.swift
@@ -35,8 +35,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        let userData = ["User":"Admin", "Permission":"Full"]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else {return}
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { data, response, error in
+            guard let data = data,
+                  let response = response else {return}
+            
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-    
-    
 }
 
